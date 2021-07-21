cb7e08ea6674ddc3e8ade64a68095ee6c21fde00
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-07-22 03:38:54 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading";
+import UserList from "./UserList";
+
+export default function Home() {
+  return (
+    <>
+      <Heading />
+      <UserList />
+    </>
+  );
+}
